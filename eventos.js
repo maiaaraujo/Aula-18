bb10ea597ea51759e24f12521ccd41fb4be5a75b
LucasDCoelho <jessicamaiaaraujo@gmail.com>
2023-02-23 11:57:24 -0300

    javascript

--- eventos.js
+++ eventos.js
@@ -1,4 +1,14 @@
 function posterior()
 {
-    
+    let itemAtivo = document.querySelector (".active");
+    let posicaoAtiva = itemAtivo.getAttribute ("posicao") + 1;
+
+    let todosItems = document.querySelectorAll("li");
+    for(let i = 0; i < todosItems.length; i++){
+        if (todosItems[i].getAttribute("posicao") == posicaoAtiva){
+            todosItems[i].classList.add("active")
+        }else{
+            todosItems[i].classList.remove("active")
+        }
+    }
 }
