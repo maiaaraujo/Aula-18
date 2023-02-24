068d710ef7869760b2ea8759ac3a193ad6499ee9
LucasDCoelho <jessicamaiaaraujo@gmail.com>
2023-02-24 08:40:57 -0300

    aula 18.1 função button

--- eventos.js
+++ eventos.js
@@ -1,9 +1,17 @@
 function posterior()
 {
     let itemAtivo = document.querySelector (".active");
-    let posicaoAtiva = itemAtivo.getAttribute ("posicao") + 1;
-
+    let posicaoAtiva = itemAtivo.getAttribute ("posicao");
     let todosItems = document.querySelectorAll("li");
+    if(posicaoAtiva == todosItems.length){
+
+        posicaoAtiva = 1;
+    }else{
+        posicaoAtiva++;
+    }
+
+
+    
     for(let i = 0; i < todosItems.length; i++){
         if (todosItems[i].getAttribute("posicao") == posicaoAtiva){
             todosItems[i].classList.add("active")
