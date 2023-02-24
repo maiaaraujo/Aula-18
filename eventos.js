40f31e7098444de6d05657623b25710cdccd3986
LucasDCoelho <jessicamaiaaraujo@gmail.com>
2023-02-24 09:04:28 -0300

    botao teste

--- eventos.js
+++ eventos.js
@@ -20,3 +20,26 @@ function posterior()
         }
     }
 }
+
+function anterior()
+{
+    let itemAtivo = document.querySelector (".active");
+    let posicaoAtiva = itemAtivo.getAttribute ("posicao");
+    let todosItems = document.querySelectorAll("li");
+    if(posicaoAtiva == 1){
+
+        posicaoAtiva = todosItems.length;
+    }else{
+        posicaoAtiva--;
+    }
+
+
+    
+    for(let i = 0; i < todosItems.length; i++){
+        if (todosItems[i].getAttribute("posicao") == posicaoAtiva){
+            todosItems[i].classList.add("active")
+        }else{
+            todosItems[i].classList.remove("active")
+        }
+    }
+}
